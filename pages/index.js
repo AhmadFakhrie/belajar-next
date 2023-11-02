a8038cf54bf161d08926480bf39d7f771a54f3f3
Ahmad Fakhrie <ahmadfakhrie07@gmail.com>
2023-11-02 10:14:45 +0700

    main

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'  //test
+import styles from '@/styles/Home.module.css'
+// import Navbar from "./components/navbar.js"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+  // <>
+  //   <Navbar />
+  // </>
     <>
       <Head>
         <title>Create Next App</title>
@@ -110,5 +114,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
